ef02800e89fc85d3b12712fa695ac49735e09009
Udhay <udhayapsg@gmail.com>
2022-07-03 04:01:28 +0530

    Product Card design updated, but flex not added yet for parent

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { Card, Button } from 'react-bootstrap';
+import './Product.css'
 
 const Product = (props) => {
     const { product } = props;
+
+    const placeholderImagePath = 'images/placeholder.png';
+
     return (
-        <div>
-            <p>{product.name}</p>
-            <p>{product.mrp}</p>
-            <p>{product.discount}</p>
-            <p>{product.discountedSellingPrice}</p>
-        </div>
+
+        <Card className="cardClass">
+            <div style={{textAlign: 'center'}}>
+            <Card.Img  className="card-image" variant="top" src={placeholderImagePath} />
+            </div>
+        
+            <Card.Body>
+            <div className="card-text-body">
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{product.mrp}</Card.Text>
+                <Card.Text>{product.discount}%</Card.Text>
+                <Card.Title>{product.discountedSellingPrice}</Card.Title>
+                <Button className="card-btn" variant="primary">Add to Cart</Button>
+            </div>
+            </Card.Body>
+        </Card>
     )
 }
 
